1d089790c57875ca11eef6a0f1673614abdaebf4
Alexandre Mezeray <alexandre.mezeray@desjardins.com>
2021-05-07 14:03:03 -0400

    Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   {path: 'admin', component: AdminComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)},
   {path: '**', component: ErrorComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {ProductsModule} from './products/products.module';
 import {SharedModule} from './shared/shared.module';
 import {AppRoutingModule} from './app-routing.module';
 
@@ -16,7 +15,6 @@ import {AppRoutingModule} from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    ProductsModule,
     SharedModule,
     AppRoutingModule
   ],
